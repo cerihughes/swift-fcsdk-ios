e0209c341e47d4beb25cea8859d6ab8dc1847ecf
cerihughes <hughesceri+github@gmail.com>
2023-08-08 10:51:40 +0100

    Try bundled lib version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/uhinc3doyeo389aiyjb2v/FCSDKiOS-4.2.5-Xcode4_2-Intel-iOS11-1.xcframework.zip?rlkey=929t0zvykxx1db3mgky0pupok&dl=1", checksum: "8e93fe513804cb1cdc2a5e191cb166d0fbab3eb3fe2b2d7876f9cb7180a5bfa8"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/3vzavapzh352q6wxtaccv/FCSDKiOS-4.2.5-Xcode14_3_1-M1-iOS13-Bundled_stdlib-1.xcframework.zip?rlkey=ur7d305vpcka8c3q2007l3226&dl=1", checksum: "cb7bda6fddb29090330ead1fde52e82ee0aae79fab2258d038814253ffb98a58"),
+//        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/uhinc3doyeo389aiyjb2v/FCSDKiOS-4.2.5-Xcode4_2-Intel-iOS11-1.xcframework.zip?rlkey=929t0zvykxx1db3mgky0pupok&dl=1", checksum: "8e93fe513804cb1cdc2a5e191cb166d0fbab3eb3fe2b2d7876f9cb7180a5bfa8"),
 //        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/krfris76fglgskndplv7f/FCSDKiOS-4.2.5-Xcode14_3_1-M1-iOS11-1.xcframework.zip?rlkey=moyvb1h1mywwpapq2tkn23xt5&dl=1", checksum: "e6583f4393071cbdc596219336f4988e721e92007a0a33e348ce1fcc8a51021f"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
