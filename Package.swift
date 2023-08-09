d5a4fe182095a4e52b213bb5f34b4c7b8c1f862a
cerihughes <hughesceri+github@gmail.com>
2023-08-09 10:44:18 +0100

    Try Cole's latest and greatest

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-ios-13-min.xcframework.zip", checksum: "669db22d972204ab55dece8b569615660d9277a86403fc6266023ac17e506035"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-latest-code-for-production.xcframework.zip", checksum: "b2b1213e383b418765e553c2f093db080e2cca66fdddd0e0591b7d1b46b94c52"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
