abb01c1d777c94494f2e08f23ef672f67fbf91aa
cerihughes <hughesceri+github@gmail.com>
2023-08-09 11:44:11 +0100

    Try pure-network-availability

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-all-ios-13-repos.xcframework.zip", checksum: "a03246b84b822a02daec40f549b2884c1636a0d243bd29771b8c7e66fbdc71e4"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-ios-13-repos-with-pure-network-availability.xcframework.zip", checksum: "9d3250f949fb58762c8964d4277536ae8d4d99a0039de9613bec15214df56da7"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
