e28db99f4d06ca92f89b2955a5050c8cf05867af
cerihughes <hughesceri+github@gmail.com>
2023-08-09 13:22:58 +0100

    Revert to locally built version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-swift-concurrency-pure-network.xcframework.zip", checksum: "ce6047d145233d977f3ef788f7ae7a226edd1e7717c595c337708dd729053bc2"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/p0k2xpdyqzx282efw9vjc/FCSDKiOS-4.2.5-Xcode14_3_1-M1-iOS11_built_against_iOS13-1.xcframework.zip?rlkey=z5ovga4v8h3yzohaekvsl3htf&dl=1", checksum: "99210148fc26467559da0eb1d060fb2e517ad55f77e80a7291b90d9bb54a4c91"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
