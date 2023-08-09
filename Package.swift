6a7619d3e65dea36dbf365bdd1cd6972143b6d77
cerihughes <hughesceri+github@gmail.com>
2023-08-09 11:59:34 +0100

    Use swift-concurrency-pure-network version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-ios-13-repos-with-pure-network-availability.xcframework.zip", checksum: "9d3250f949fb58762c8964d4277536ae8d4d99a0039de9613bec15214df56da7"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5-swift-concurrency-pure-network.xcframework.zip", checksum: "ce6047d145233d977f3ef788f7ae7a226edd1e7717c595c337708dd729053bc2"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
