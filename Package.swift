b1f3535851e3f585074b98422bb676660db0f01b
cerihughes <hughesceri+github@gmail.com>
2023-08-10 11:02:22 +0100

    4.2.5 RC

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 "FCSDKiOS",
                 "WebRTC"
             ]),
-        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/p0k2xpdyqzx282efw9vjc/FCSDKiOS-4.2.5-Xcode14_3_1-M1-iOS11_built_against_iOS13-1.xcframework.zip?rlkey=z5ovga4v8h3yzohaekvsl3htf&dl=1", checksum: "99210148fc26467559da0eb1d060fb2e517ad55f77e80a7291b90d9bb54a4c91"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://www.dropbox.com/scl/fi/t6v07dy0cw1ji3jdaqs3c/FCSDKiOS-4.2.5-Xcode14_3_1-M1-iOS11-use-correct-cbats-branch.xcframework.zip?rlkey=a67ku4v0bf0fskh7vcskgfkks&dl=1", checksum: "a51a83519da038b326b591f4fd2b1e868b448b48fb7432240bb8c11fc394e66d"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
